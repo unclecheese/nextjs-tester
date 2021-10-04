ee38ffa9ee2899613693a1924eba56042baf5d9b
Aaron Carlino <unclecheese@leftandmain.com>
2021-10-05 11:27:15 +1300

    debug

--- pages/[[...page]].tsx
+++ pages/[[...page]].tsx
@@ -4,14 +4,27 @@ import { createPage } from "@silverstripe/nextjs-toolkit"
 import {
   getStaticProps as createGetStaticProps,
   getStaticPaths as createGetStaticPaths,
+  getCacheDir,
 } from "@silverstripe/nextjs-builder"
-
+import fs from "fs"
+// @ts-ignore
+import glob from "glob"
 const project = {
   projectConfig,
   cacheManifest,
 }
 
-export const getStaticProps = createGetStaticProps(project)
+const d = createGetStaticProps(project)
+export const getStaticProps = async (context: any) => {
+
+  if (!fs.existsSync(`${getCacheDir()}/.availableTemplates.json`)) {
+    const f = glob.sync(`${getCacheDir()}/*.*`, { dot: true })
+    throw new Error(`Whoa there... ${JSON.stringify(f)}`)
+  }
+  const r = await d(context)
+
+  return r
+}
 export const getStaticPaths = createGetStaticPaths(project)
 
 const Page = createPage(project)
