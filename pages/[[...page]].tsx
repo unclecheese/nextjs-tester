4bc0c5712721cc1899c9d302bd0cc3e9f11ff5ae
Aaron Carlino <unclecheese@leftandmain.com>
2021-10-05 11:18:47 +1300

    debug

--- pages/[[...page]].tsx
+++ pages/[[...page]].tsx
@@ -5,22 +5,13 @@ import {
   getStaticProps as createGetStaticProps,
   getStaticPaths as createGetStaticPaths,
 } from "@silverstripe/nextjs-builder"
-// @ts-ignore
-import glob from "glob"
-import { getCacheDir } from "@silverstripe/nextjs-builder"
+
 const project = {
   projectConfig,
   cacheManifest,
 }
 
-const d = createGetStaticProps(project)
-export const getStaticProps = async (context: any) => {  
-  const files = glob.sync(`${getCacheDir()}/*.json`, { dot: true })
-  throw new Error(JSON.stringify(files))
-  const r = await d(context)
-
-  return r
-}
+export const getStaticProps = createGetStaticProps(project)
 export const getStaticPaths = createGetStaticPaths(project)
 
 const Page = createPage(project)
